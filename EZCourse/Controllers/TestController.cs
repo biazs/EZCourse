db6399c6371c030892953b7ef59408ec22471f66
Bia <fabianazs@gmail.com>
2019-09-27 14:53:06 -0300

    Controllers

---
+++ EZCourse/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EZCourse.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult JsonOut(int id, Person person)
+        {
+            var obj = new
+            {
+                Id = id,
+                Person = person
+            };
+            return Json(obj);
+        }
+
+        public class Person
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+
+    }
+}
