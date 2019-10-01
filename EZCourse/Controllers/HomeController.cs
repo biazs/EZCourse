a85126078d7db4cbae7137bcc29e86169c9e0836
Bia <fabianazs@gmail.com>
2019-10-01 09:41:05 -0300

    Services and Dependency Injection

--- EZCourse/Controllers/HomeController.cs
+++ EZCourse/Controllers/HomeController.cs
@@ -5,11 +5,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using EZCourse.Models;
+using MailKit.Net.Smtp;
+using MimeKit;
+using EZCourse.Services;
 
 namespace EZCourse.Controllers
 {
     public class HomeController : Controller
     {
+        readonly Services.Smtp _smtpService;
+
+        public HomeController(Services.Smtp smtpService)
+        {
+            _smtpService = smtpService;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -37,7 +47,34 @@ namespace EZCourse.Controllers
             {
                 return View(formData);
             }
-            return Json(formData);
+
+            var htmlBody = $"<p>{formData.Name} ({formData.Email})</p><p>{formData.Phone}</p><p>{formData.Message}</p>";
+            var textBody = "{formData.Name} {formData.Email})\r\n{formData.Phone}\r\n{formData.Message}";
+
+            _smtpService.SendSingle("Contact Form", htmlBody, textBody, "RM", "contact@domain.com",
+                                    "noreply mysite", "noreply@domain");
+
+            /* using (var client = new SmtpClient())
+             {
+                 client.Connect("mail.domain.com");
+                 client.Authenticate("username", "password");
+                 var bodyBuilder = new BodyBuilder();
+                 bodyBuilder.HtmlBody = $"<p>{formData.Name} ({formData.Email})</p><p>{formData.Phone}</p><p>{formData.Message}</p>";
+                 bodyBuilder.TextBody = "{formData.Name} {formData.Email})\r\n{formData.Phone}\r\n{formData.Message}";
+
+                 var message = new MimeMessage();
+                 message.Body = bodyBuilder.ToMessageBody();
+                 message.From.Add(new MailboxAddress("noreply mysite", "noreply@video.com"));
+                 message.To.Add(new MailboxAddress("RM", "contact@domain.com"));
+                 message.Subject = "Contact Form";
+                 client.Send(message);
+
+                 client.Disconnect(true);
+
+             }*/
+
+            TempData["Message"] = "Thank you! Your message is sent to us";
+            return RedirectToAction("Contact");
 
         }
 
---
+++ EZCourse/Services/Smtp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+
+namespace EZCourse.Services
+{
+    public class Smtp
+    {
+        public void SendSingle(string subject, string htmlBody, string textBody,
+            string toName, string toAddress, string fromName, string fromAddress)
+        {
+            /*  using (var client = new SmtpClient())
+             {
+                 client.Connect("mail.domain.com");
+                 client.Authenticate("username", "password");
+                 var bodyBuilder = new BodyBuilder();
+                 bodyBuilder.HtmlBody = htmlBody;
+                 bodyBuilder.TextBody = textBody;
+
+                 var message = new MimeMessage();
+                 message.Body = bodyBuilder.ToMessageBody();
+                 message.From.Add(new MailboxAddress(fromName, fromAddress));
+                 message.To.Add(new MailboxAddress(toName, toAddress));
+                 message.Subject = subject;
+                 client.Send(message);
+
+                 client.Disconnect(true); 
+             } */
+        }
+
+    }
+}
